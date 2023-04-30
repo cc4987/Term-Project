dd7b9e29fb455c0af841a64628693bd122c5045b
cc4987 <98005317+cc4987@users.noreply.github.com>
2023-04-30 06:34:20 -0400

    .

--- app.py
+++ app.py
@@ -5,4 +5,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'please enter /movie for the movie recs tool!'
+    return 'READ.md'
+
+@app.get('/movie')
+def get_movie():
+    return render_template('index.html')
+@app.post('/movie')
+def movie_post():
+    movie_search = request.form.get('movie_name')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
